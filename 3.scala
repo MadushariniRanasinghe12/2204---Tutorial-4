ba16b3e5da14e51c4055643eb5dd064413c83178
MadushariniRanasinghe12 <87421056+MadushariniRanasinghe12@users.noreply.github.com>
2022-08-01 21:33:24 +0530

    Update 3.scala

--- 3.scala
+++ 3.scala
@@ -13,8 +13,8 @@ def main(args:Array[String]) = {
     }
 
     println(formatNames("Benny")(toUpper(_)))
-    println(formatNames("Niroshan")(toLower(_)))
+    println(formatNames("Niroshan".substring(0,2))(toUpper(_)) + formatNames("Niroshan".substring(2))(toLower(_)))
     println(formatNames("Saman")(toLower(_)))
-    println(formatNames("Kumara")(toUpper(_)))
+    println(formatNames("Kumara".substring(0,1))(toUpper(_)) + formatNames("Kumara".substring(1,5))(toLower(_)) + formatNames("Kumara".substring(5))(toUpper(_)))
 
 }
