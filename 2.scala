1b1613b62ca0835de9bb0357fa3c1d8a7bfc0b7d
MadushariniRanasinghe12 <87421056+MadushariniRanasinghe12@users.noreply.github.com>
2022-08-02 09:50:04 +0530

    Update 2.scala

--- 2.scala
+++ 2.scala
@@ -1,16 +1,15 @@
 def main(args:Array[String]) = {
 
-    def Patternmatching(number: Integer): Unit = number match{
-        case x if x<=0 => print("\nNegative/Zero number is given")
-        case x if x%2 == 0 => print("\nEven number is given")
-        case x if x%2 != 0 => print("\nOdd number is given")
+    def Patternmatching(number: Int): String = number match{
+        case x if x<=0 => "Negative/Zero number is given"
+        case x if x%2 == 0 => "Even number is given"
+        case x if x%2 != 0 => "Odd number is given"
        
     }
 
-    Patternmatching(-20)
-    Patternmatching(5)
-    Patternmatching(10)
+    print("Enter the number :");
+    val num = scala.io.StdIn.readInt();
+    val answer = Patternmatching(num);
+    printf(answer);
       
-
-    
 }
